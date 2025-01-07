e9cbe12404c7284b0352d20f724ce1e4c10cfb22
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-01-07 08:56:15 -0300

    adicionando token ao logar

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import { pool } from "./mysql.js";
-import bcrypt from "bcrypt";
+import bcrypt, { compare } from "bcrypt";
+import Jwt from "jsonwebtoken";
+
+interface Typegem {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  token: string;
+  expiresIn: string;
+}
 
 const app = express();
 app.use(express.json());
@@ -13,7 +23,7 @@ app.post("/rota", (req, res) => {
     const passwordHash = bcrypt.hashSync(password, 10);
 
     const sql =
-      "INSERT INTO users (`user-id`, name, email, password) VALUES(?,?,?,?)";
+      "INSERT INTO users (user_id, name, email, password) VALUES(?,?,?,?)";
     pool.query(sql, [id, name, email, passwordHash], (err, result) => {
       if (err) {
         return res.status(400).json("Erro ao conectar ao banco de dados");
@@ -24,4 +34,45 @@ app.post("/rota", (req, res) => {
     res.status(400).json("Erro ao criptgrafar senha");
   }
 });
+
+app.post("/sign-in", (req, res) => {
+  const { email, password } = req.body;
+
+  const sql = "SELECT * FROM users WHERE email = ?";
+  pool.query(sql, [email], (err, results) => {
+    console.log(results[0]);
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "Erro ao acessar o banco de dados" });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    // Usuário encontrado, comparar senhas
+    const user = results[0];
+    compare(password, user.password, (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: "Erro ao comparar senha" });
+      }
+
+      if (result) {
+        const token = Jwt.sign(
+          {
+            id: results[0].user_id,
+            email: results[0].email,
+          },
+          "segredo",
+          { expiresIn: "1h" }
+        );
+
+        console.log(token);
+        return res.status(200).json({ token });
+      }
+    });
+  });
+});
+
 app.listen(4000);
